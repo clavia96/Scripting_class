41bf1b4d27740726de7a4d5a5fa6c109a5276d8a
Beatrice Severance <bms0068@auburn.edu>
2020-04-10 15:30:58 -0500

    uncommented lines

--- Script.bash
+++ Script.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-module load genometools
 module load anaconda/3-2020.02
 
 #use user input and strip white space
@@ -31,8 +30,8 @@ case "$ext" in
     ;;
 esac
 
-#gt suffixerator -db $infile -indexname $infile -tis -suf -lcp -des -ssp -sds -dna
-#gt ltrharvest -index $infile -seqids yes -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -similar 85 -vic 10 -seed 20 -motif TGCA -motifmis 1 > $infile.harvest.scn
-#gt ltrharvest -index $infile -seqids yes -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -similar 85 -vic 10 -seed 20 > $infile.harvest.nonTGCA.scn
+gt suffixerator -db $infile -indexname $infile -tis -suf -lcp -des -ssp -sds -dna
+gt ltrharvest -index $infile -seqids yes -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -similar 85 -vic 10 -seed 20 -motif TGCA -motifmis 1 > $infile.harvest.scn
+gt ltrharvest -index $infile -seqids yes -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -similar 85 -vic 10 -seed 20 > $infile.harvest.nonTGCA.scn
 
-#ltr_finder -D 15000 -d 1000 -l 100 -L 7000 -p 20 -C -M 0.85 $infile > $infile.finder.scn
+ltr_finder -D 15000 -d 1000 -l 100 -L 7000 -p 20 -C -M 0.85 $infile > $infile.finder.scn
