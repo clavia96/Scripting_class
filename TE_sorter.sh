72ed46b26d4df9bfc04d72732a395cdea845ce1c
clavia96 <51423271+clavia96@users.noreply.github.com>
2020-04-19 19:24:27 -0500

    Delete TE_sorter.sh

--- TE_sorter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load python/2.7.15
-module load hmmer/3.1b2
-module load ncbi-blast/2.2.31+
-
-
-python /home/olo0002/TEsorter/TEsorter.py -db gydb -st nucl -p 10 genome_pass_list.fa
